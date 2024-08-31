d3c3f46ffa62304c536bb37afb19f64a77cb1cca
Tushar <tusharvashisth4@gmail.com>
2024-08-31 18:52:43 +0530

    did some changes in redis configuration

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,7 +1,15 @@
 import { Redis } from "ioredis";
-const redis = new Redis({
-  host: "localhost",
-  port: 6379,
-});
+let redis: Redis;
+if (process.env.NODE_ENV === "production") {
+  redis = new Redis(
+    "rediss://default:AbAPAAIjcDFlNzVhZjI3MDA4NDk0N2M5ODNmNWQzMDI1ZjY5OWJiNnAxMA@engaging-seagull-45071.upstash.io:6379"
+  );
+} else {
+  redis = new Redis({
+    host: "localhost",
+    port: 6379,
+  });
+}
+
 
 export default redis;
